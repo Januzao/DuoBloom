fef4f5ffac540a9ca4ac47de518f5492e019b84f
Denys <146959885+fxtortic@users.noreply.github.com>
2026-03-08 17:35:18 +0200

    setup angular capacitor android frontend

---
+++ frontend/android/app/src/main/java/com/duobloom/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.duobloom.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.duobloom.app',
+  appName: 'DuoBloom',
+  webDir: 'dist/frontend/browser'
+};
+
+export default config;
