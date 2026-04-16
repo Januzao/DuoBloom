ecaca9e65cbcba06f887395202a4989a85e4c5d8
golembio <golembjovskijm@gmail.com>
2026-04-16 14:24:36 +0200

    Setup project with env variables

---
+++ backend/src/main/java/org/example/duobloom/DuoBloomApplication.java
@@ -0,0 +1,13 @@
+package org.example.duobloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuoBloomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuoBloomApplication.class, args);
+    }
+
+}
